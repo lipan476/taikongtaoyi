68251b9f3724629ba0eb27198b59e6c7fe7a800d
lipan <670339228@qq.com>
2025-04-28 17:28:21 +0800

    更新代码

--- application.0cc55.js
+++ application.0cc55.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.061d1.json';
+          this.settingsPath = 'src/settings.46e1d.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.2d57a.js
+++ index.2d57a.js
@@ -1,4 +1,4 @@
-System.register(["./application.cef5f.js"], function (_export, _context) {
+System.register(["./application.0cc55.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
